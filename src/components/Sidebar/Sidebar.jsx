53a73d14d1bc74024950c8e8c3a1026674541295
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2018-12-09 00:03:34 +0100

    add .editorconfig 🏹

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import './style.sass';
 
 export default class Sidebar extends React.Component {
-    state = { isOpen: false }
+  state = { isOpen: false }
 
-    render() {
-        return (
-            <div className={`Sidebar ${this.state.isOpen ? 'Sidebar--open' : ''}`}>
-                <label className="Sidebar__handle" aria-hidden>
-                    <input hidden checked={this.state.isOpen} type="checkbox" onChange={(e) => {
-                        this.setState({ isOpen: !this.state.isOpen })
-                    }} />
-                </label>
-                <div className="Sidebar__content">
-                    {this.props.children}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={`Sidebar ${this.state.isOpen ? 'Sidebar--open' : ''}`}>
+        <label className="Sidebar__handle" aria-hidden>
+          <input hidden checked={this.state.isOpen} type="checkbox" onChange={(e) => {
+            this.setState({ isOpen: !this.state.isOpen })
+          }} />
+        </label>
+        <div className="Sidebar__content">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
 }

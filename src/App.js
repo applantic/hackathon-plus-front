8b008861b679323891008f38a8564c9b892b8a23
maciejmatu <maciek.matuszewski@gmail.com>
2018-12-08 20:16:35 +0100

    Add styling for maps and dynamic place selection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
+/* globals google */
 import React, { Component } from "react";
 import "./App.css";
 
 import GoogleMaps from "./components/GoogleMaps";
 import LocationSearchInput from "./components/LocationSearchInput/LocationSearchInput";
 class App extends Component {
+  DirectionsService = new google.maps.DirectionsService();
+  state = {
+    directions: null,
+    startGeo: null,
+    endGeo: null
+  };
+
+  getDirections = () => {
+    const { startGeo, endGeo } = this.state;
+    if (!startGeo || !endGeo) return;
+
+    this.DirectionsService.route(
+      {
+        origin: new google.maps.LatLng(startGeo.lat, startGeo.lng),
+        destination: new google.maps.LatLng(endGeo.lat, endGeo.lng),
+        travelMode: google.maps.TravelMode.TRANSIT
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.setState({
+            directions: result
+          });
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
         <header className="header">
           <LocationSearchInput
+            onChange={startGeo => this.setState({ startGeo })}
             placeholder="Jadę z ..."
           />
           <LocationSearchInput
+            onChange={endGeo => this.setState({ endGeo })}
             placeholder="Jadę do ..."
           />
-          <button className="button">Sprawdź</button>
+          <button className="button" onClick={this.getDirections}>
+            Sprawdź
+          </button>
         </header>
         <main>
-          <GoogleMaps />
+          <GoogleMaps directions={this.state.directions} />
         </main>
-        <footer>
-          <div>We Make Buttons</div>
+        <footer className="footer">
+          <p>Made with</p>
+          <span
+            role="img"
+            aria-label="serce"
+            className="emoji"
+          >
+            ❤️
+          </span>
+          <p>by</p>
+          <a
+            className="link-to-authors"
+            href="https://applantic.github.io/wemakebuttons/"
+          >
+            We Make Buttons - Hackaton Team
+          </a>
+          <p>in</p>
+          <span
+            role="img"
+            aria-label="flaga polski"
+            className="emoji"
+          >
+            🇵🇱
+          </span>
+          <p>2018</p>
         </footer>
       </div>
     );
--- src/components/GoogleMaps.jsx
+++ src/components/GoogleMaps.jsx
@@ -1,45 +1,66 @@
-/* globals google */
 import React from "react";
-import { compose, withProps, lifecycle } from "recompose";
+import { compose, withProps } from "recompose";
+import mapsConfig from "./mapsConfig";
 import {
   withGoogleMap,
   GoogleMap,
   DirectionsRenderer
 } from "react-google-maps";
 
-const MapWithADirectionsRenderer = compose(
+class MapWithADirectionsRenderer extends React.PureComponent {
+  render() {
+    const { directions } = this.props;
+
+    return (
+      <GoogleMap defaultOptions={{ styles: mapsConfig }} defaultZoom={7}>
+        {directions && <DirectionsRenderer directions={directions} />}
+      </GoogleMap>
+    );
+  }
+}
+
+export default compose(
   withProps({
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `calc(100vh - 200px)` }} />,
+    containerElement: <div style={{ height: `calc(100vh - 220px)` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
-  withGoogleMap,
-  lifecycle({
-    componentDidMount() {
-      const DirectionsService = new google.maps.DirectionsService();
+  withGoogleMap
+)(MapWithADirectionsRenderer);
 
-      DirectionsService.route(
-        {
-          origin: "Warsaw",
-          destination: "Cracow",
-          travelMode: google.maps.TravelMode.TRANSIT
-        },
-        (result, status) => {
-          if (status === google.maps.DirectionsStatus.OK) {
-            this.setState({
-              directions: result
-            });
-          } else {
-            console.error(`error fetching directions ${result}`);
-          }
-        }
-      );
-    }
-  })
-)(props => (
-  <GoogleMap defaultZoom={7}>
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
-  </GoogleMap>
-));
+// const MapWithADirectionsRenderer = compose(
+// withProps({
+//   loadingElement: <div style={{ height: `100%` }} />,
+//   containerElement: <div style={{ height: `calc(100vh - 200px)` }} />,
+//   mapElement: <div style={{ height: `100%` }} />
+// }),
+//   withGoogleMap,
+//   lifecycle({
+//     componentDidMount() {
+//       const DirectionsService = new google.maps.DirectionsService();
 
-export default MapWithADirectionsRenderer;
+//       DirectionsService.route(
+//         {
+//           origin: "Warsaw",
+//           destination: "Cracow",
+//           travelMode: google.maps.TravelMode.TRANSIT
+//         },
+//         (result, status) => {
+//           if (status === google.maps.DirectionsStatus.OK) {
+//             this.setState({
+//               directions: result
+//             });
+//           } else {
+//             console.error(`error fetching directions ${result}`);
+//           }
+//         }
+//       );
+//     }
+//   })
+// )(props => (
+//   <GoogleMap defaultZoom={7}>
+//     {props.directions && <DirectionsRenderer directions={props.directions} />}
+//   </GoogleMap>
+// ));
+
+// export default MapWithADirectionsRenderer;

e2519f6d7befaa087f02c701669cb25123c9e381
maciejmatu <maciek.matuszewski@gmail.com>
2018-12-08 18:50:58 +0100

    Add places

--- src/components/GoogleMaps.jsx
+++ src/components/GoogleMaps.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { compose, withProps, lifecycle } from "recompose";
 import {
-  withScriptjs,
   withGoogleMap,
   GoogleMap,
   DirectionsRenderer
@@ -10,14 +9,10 @@ import {
 
 const MapWithADirectionsRenderer = compose(
   withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${
-      process.env.REACT_APP_GOOGLE_MAPS
-    }&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `calc(100vh - 96px)` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
-  withScriptjs,
   withGoogleMap,
   lifecycle({
     componentDidMount() {
@@ -25,9 +20,9 @@ const MapWithADirectionsRenderer = compose(
 
       DirectionsService.route(
         {
-          origin: new google.maps.LatLng(41.85073, -87.65126),
-          destination: new google.maps.LatLng(41.85258, -87.65141),
-          travelMode: google.maps.TravelMode.DRIVING
+          origin: "Warsaw",
+          destination: "Cracow",
+          travelMode: google.maps.TravelMode.TRANSIT
         },
         (result, status) => {
           if (status === google.maps.DirectionsStatus.OK) {
@@ -42,46 +37,9 @@ const MapWithADirectionsRenderer = compose(
     }
   })
 )(props => (
-  <GoogleMap
-    defaultZoom={7}
-    defaultCenter={new google.maps.LatLng(41.85073, -87.65126)}
-  >
+  <GoogleMap defaultZoom={7}>
     {props.directions && <DirectionsRenderer directions={props.directions} />}
   </GoogleMap>
 ));
 
 export default MapWithADirectionsRenderer;
-
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-// class GoogleMaps extends Component {
-//   static defaultProps = {
-//     center: {
-//       lat: 59.95,
-//       lng: 30.33
-//     },
-//     zoom: 11
-//   };
-
-//   render() {
-//     return (
-//       // Important! Always set the container height explicitly
-//       <div style={{ height: "calc(100vh - 96px)", width: "100%" }}>
-//         {/* <GoogleMapReact
-//           bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS }}
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}
-//         >
-//           <AnyReactComponent
-//             lat={59.955413}
-//             lng={30.337844}
-//             text={"Kreyser Avrora"}
-//           />
-//         </GoogleMapReact> */}
-
-//       </div>
-//     );
-//   }
-// }
-
-// export default GoogleMaps;

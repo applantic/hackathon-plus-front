1ed14de0ca3d3e98e5e1a95565640c6f2af569f9
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2018-12-08 16:59:56 +0100

    GoogleMaps 🗺

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import GoogleMaps from './components/GoogleMaps'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="container">
           <button className="button">
             We Make Buttons
           </button>
+          <GoogleMaps />
         </header>
       </div>
     );
---
+++ src/components/GoogleMaps.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class GoogleMaps extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyD7bHuYvn93LncMeUOqLTPua81P0rbxfmQ' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default GoogleMaps;

5547cd57d6194f969629c3d059e650b8b931fc9a
maciejmatu <maciek.matuszewski@gmail.com>
2018-12-08 20:16:35 +0100

    Add styling for maps and dynamic place selection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+/* globals google */
 import React, { Component } from "react";
 import "./App.css";
 
 import GoogleMaps from "./components/GoogleMaps";
 import LocationSearchInput from "./components/LocationSearchInput/LocationSearchInput";
 class App extends Component {
+  DirectionsService = new google.maps.DirectionsService();
+  state = {
+    directions: null,
+    startGeo: null,
+    endGeo: null
+  };
+
+  getDirections = () => {
+    const { startGeo, endGeo } = this.state;
+    if (!startGeo || !endGeo) return;
+
+    this.DirectionsService.route(
+      {
+        origin: new google.maps.LatLng(startGeo.lat, startGeo.lng),
+        destination: new google.maps.LatLng(endGeo.lat, endGeo.lng),
+        travelMode: google.maps.TravelMode.TRANSIT
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.setState({
+            directions: result
+          });
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
         <header className="header">
           <LocationSearchInput
+            onChange={startGeo => this.setState({ startGeo })}
             placeholder="Jadę z ..."
           />
           <LocationSearchInput
+            onChange={endGeo => this.setState({ endGeo })}
             placeholder="Jadę do ..."
           />
-          <button className="button">Sprawdź</button>
+          <button className="button" onClick={this.getDirections}>
+            Sprawdź
+          </button>
         </header>
         <main>
-          <GoogleMaps />
+          <GoogleMaps directions={this.state.directions} />
         </main>
         <footer>
           <div>We Make Buttons</div>
--- src/components/LocationSearchInput/LocationSearchInput.jsx
+++ src/components/LocationSearchInput/LocationSearchInput.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import PlacesAutocomplete, {
   geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
+  getLatLng
+} from "react-places-autocomplete";
 
-import './style.sass'
+import "./style.sass";
 
 export default class LocationSearchInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { address: '' };
+    this.state = { address: "" };
   }
 
   handleChange = address => {
@@ -18,9 +18,16 @@ export default class LocationSearchInput extends React.Component {
 
   handleSelect = address => {
     geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error));
+      .then(results => {
+        this.setState({ address: results[0].formatted_address });
+        return getLatLng(results[0]);
+      })
+      .then(latLng => {
+        this.props.onChange && this.props.onChange(latLng);
+      })
+      .catch(error => {
+        console.error("Error", error);
+      });
   };
 
   render() {
@@ -35,24 +42,24 @@ export default class LocationSearchInput extends React.Component {
             <input
               {...getInputProps({
                 placeholder: this.props.placeholder,
-                className: 'location-search-input',
+                className: "location-search-input"
               })}
             />
             <div className="autocomplete-dropdown-container absolute">
               {loading && <div>Loading...</div>}
               {suggestions.map(suggestion => {
                 const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
+                  ? "suggestion-item--active"
+                  : "suggestion-item";
                 // inline style for demonstration purpose
                 const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                  : { backgroundColor: "#ffffff", cursor: "pointer" };
                 return (
                   <div
                     {...getSuggestionItemProps(suggestion, {
                       className,
-                      style,
+                      style
                     })}
                   >
                     <span>{suggestion.description}</span>

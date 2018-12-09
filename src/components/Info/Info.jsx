0aba83ec9a9771c8a1226de33d10b081da9e7f29
maciejmatu <maciek.matuszewski@gmail.com>
2018-12-09 12:27:55 +0100

    Display correct icons

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,6 +1,65 @@
 import React from "react";
 import "./style.sass";
 
+const chosen_polish_keys = {
+  // Kasa_dla_PRM: {
+  //   text: "Kasa dla PRM",
+  //   namePath: "CASH_REG_FOR_DISABILITIES.jpg"
+  // },
+  Pętla_indukcyjna_w_kasie: {
+    text: "Pętla indukcyjna w kasie",
+    namePath: "INDUCTION_LOOP_IN_CASH_REG.jpg"
+  },
+  Interkom: {
+    text: "Interkom",
+    namePath: "INTERCOM.jpg"
+  },
+  Wszysc_przewoźnicy_w_kasie: {
+    text: "Wszyscy przewoźnicy w kasie",
+    namePath: "ALL_CARRIERS_TICKET_OFFICE.jpg"
+  },
+  Język_migowy_w_Infodworcu: {
+    text: "Język migowy w Infodworcu",
+    namePath: "SIGN_LANGUAGE_INFO_STATION.jpg"
+  },
+  Język_migowy_w_kasie: {
+    text: "Język migowy w kasie",
+    namePath: "SIGN_LANGUAGE_CASH_REG.jpg"
+  },
+  Urządzenia_wspomagające: {
+    text: "Urządzenia wspomagające",
+    namePath: "SUPPORTING_DEVICES.jpg"
+  },
+  WC: {
+    text: "WC",
+    namePath: "WC.jpg"
+  },
+  Usługa_asysty: {
+    text: "Usługa asysty",
+    namePath: "ASSISTANT_SERVICE.jpg"
+  },
+  Mapa_dotykowa: {
+    text: "Mapa dotykowa",
+    namePath: "TOUCH_MAP.jpg"
+  },
+  Parking_: {
+    text: "P+R,stacje postojowe",
+    namePath: "P_R_SPACE.jpg"
+  },
+  Oznakowania_poziome: {
+    text: "Oznakowania poziome",
+    namePath: "HORIZONTAL_MARKINGS.jpg"
+  },
+  Przewijak: {
+    text: "Przewijak",
+    namePath: "CHANGING.jpg"
+  },
+  Pokój_opiekuna: {
+    text: "Pokój opiekuna",
+    namePath: "GUARDIAN_ROOM.jpg"
+  }
+};
+
 export default class Info extends React.Component {
   state = {
     isOpen: []
@@ -29,8 +88,14 @@ export default class Info extends React.Component {
       const infodworzec = station["Infodworzec"];
       const oznakowania = station["Oznakowania_poziome"];
 
+      const stationInfo = Object.keys(station)
+        .filter(key => Boolean(station[key]))
+        .map(key => chosen_polish_keys[key])
+        .filter(station => Boolean(station));
+      console.log(stationInfo);
+
       return (
-        <div key={station.Id}>
+        <div key={station.Id} className="Info">
           <div className="Info__station">
             <span className="Info__time">{station.time.text}</span>
             {station.Nazwa_dworca}
@@ -54,18 +119,25 @@ export default class Info extends React.Component {
               s_index
             ) && "Info__details--open"}`}
           >
-            <ul>
-              {przewijak && <li>Posiada przewijak</li>}
-              {usluga_asysty && <li>Posiada usługę asysty</li>}
-              {a && (
-                <li>
-                  Posiada usługę pomocy z uprzedzeniem 48h
-                  <br />
-                  <a className="link-dark" href="">
-                    Zgłoś prośbę tutaj
-                  </a>
+            <ul className="Info__list">
+              {Boolean(kasa_prm) && (
+                <li className="Info__list-item">
+                  <div className="Info__list-img" />
+                  <span className="Info__list-text">Kasa PRM</span>
                 </li>
               )}
+              {stationInfo.map(info => {
+                return (
+                  <li className="Info__list-item">
+                    <img
+                      className="Info__list-img"
+                      src={"/" + info.namePath}
+                      alt={info.text}
+                    />
+                    <span className="Info__list-text">{info.text}</span>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
